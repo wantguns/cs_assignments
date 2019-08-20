f44cd2fe30d4295032cf75aa1884e16debcd5eff
csed <csed@localhost.localdomain>
2019-08-20 11:22:23 +0530

    wip asgnmt e

---
+++ assignment_1-e.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+int a, b;
+printf("Enter two integers : ");
+scanf("%d %d", &a, &b);
+printf("Sum\t:\t",a+b,"\nDifference\t:\t",a-b,"\nProduct\t:\t",a*b,"\nDivision\t:\t", a/b,"\n");
+
+float d, c;
+printf("Enter two floating numbers : ");
+scanf("%d %d", &c, &d);
+printf("Sum\t:\t",c+d,"\nDifference\t:\t",c-d,"\nProduct\t:\t",c*d,"\nDivision\t:\t", c/d,"\n");
+
+}
