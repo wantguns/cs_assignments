02e684ab967cdbaf35d54fdba03e3641b42da0ca
csed <csed@localhost.localdomain>
2019-08-20 11:55:37 +0530

    wip

---
+++ assignment_1-f.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+int p, r, t, n;
+int si, ci;
+printf("Enter principle, rate of interest and time, number of times the interest is compounded : ");
+scanf("%d %d %d %d", &p, &r, &t, &n);
+si = (p*r*t)/100;
+printf ("The Simple Interest is : ", si);
+ci = pow((p*(1 + (r/n))),(n*t));
+printf ("The Amount after compound interest is : ", ci);
+return 0;
+}
