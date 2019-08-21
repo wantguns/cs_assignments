8c5facd6ebfaeb8e5cf2182c84adc8eab4409626
wantguns <therealgunwant@gmail.com>
2019-08-21 22:43:44 +0530

    fixed

--- assignment_1/assignment_1-e.c
+++ assignment_1/assignment_1-e.c
@@ -4,17 +4,17 @@ int main(){
 int a, b;
 printf("Enter two integers : ");
 scanf("%d %d", &a, &b);
-printf("Sum : %d",a+b);
-printf("Difference : %d", a-b);
-printf("Product : %d",a*b);
-printf("Division : %d", a/b);
+printf("Sum : %d\n",a+b);
+printf("Difference : %d\n", a-b);
+printf("Product : %d\n",a*b);
+printf("Division : %d\n", a/b);
 
 float d, c;
 printf("Enter two floating numbers : ");
 scanf("%f %f", &c, &d);
-printf("Sum : %f",c+d);
-printf("Difference : %f", c-d);
-printf("Product : %f",c*d);
-printf("Division : %f", c/d);
+printf("Sum : %f\n",c+d);
+printf("Difference : %f\n", c-d);
+printf("Product : %f\n",c*d);
+printf("Division : %f\n", c/d);
 return 0;
 }
