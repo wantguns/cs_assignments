305ea652e408b2fcdcde19a6ab4e5e5d38ef4259
wantguns <therealgunwant@gmail.com>
2019-09-02 10:57:09 +0530

    started assignment 4

---
+++ assignment_4/1.c
@@ -0,0 +1,38 @@
+/*WAP to find the largest among three numbers using (i) nested if statements, (ii) else if statements,
+(iii) conditional operator*/
+#include <stdio.h>
+
+void elseif(int a, int b, int c) {
+  if (a > b) {
+    if (a > c) printf("Greatest number : %d", a);
+    else printf("Greatest number : %d", c);
+  }
+  else if (b > c) printf("Greatest number : %d", b);
+  else printf("Greatest number : %d", c);
+}
+
+void nesif(int a, int b, int c) {
+  if (a > b) {
+    if (a > c) printf("Greatest number : %d", a);
+    if (a < c) printf("Greatest number : %d", c);
+  }
+  if (a < b && b > c) printf("Greatest number : %d", b);
+  if (a < c && c > b) printf("Greatest number : %d", c);
+}
+
+void conditional(int a, int b, int c){
+  printf("Greatest number : %d\n", (a>b)?((a>c)?a:c):(b>c)?b:c);
+}
+
+int main() {
+  int a, b, c, choice;
+  printf("Enter the three numbers : ");
+  scanf("%d %d %d", &a, &b, &c);
+  printf("Choose the method you would want to find the greatest number with : \n  1. Else If\n  2. Nested If\n  3. Conditional\n");
+  scanf("%d", &choice);
+  switch(choice) {
+    case 1 : elseif(a, b, c);
+    case 2 : nesif(a, b, c);
+    case 3 : conditional(a, b, c);
+  }
+}
