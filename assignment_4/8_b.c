1ae389a2e04228c27cf8777790ec7104025bdbaf
wantguns <therealgunwant@gmail.com>
2019-09-05 11:03:30 +0530

    fixed

--- assignment_4/8_b.c
+++ assignment_4/8_b.c
@@ -5,6 +5,6 @@
 int main() {
 float a, b, c;
 printf("Enter the length of the sides of the triangle : ");
-scanf("%d%d%d", &a, &b, &c);
+scanf("%f%f%f", &a, &b, &c);
 printf((a+b>c && b+c>a && c+a>b)?"The Triangle is Valid\n":"The Triangle is invalid\n");
 }
