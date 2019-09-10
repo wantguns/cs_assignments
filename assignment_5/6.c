113da47efac298fdc45a1749891bef77131813a2
csed <csed@localhost.localdomain>
2019-09-10 11:18:30 +0530

    all done except binary (6) cuz im too lazy

--- assignment_5/6.c
+++ assignment_5/6.c
@@ -1,4 +1,3 @@
-//WIP
 
 /* WAP that accepts a decimal number and find its binary, octal and hexadecimal equivalent and vice
 versa. */
@@ -8,7 +7,7 @@ versa. */
 int main() {
     int num;
     printf("Enter a number : ");
-    scanf("%d ", &num);
+    scanf("%d", &num);
     //printf("Binary equivalent of %d =  ")
     printf("Octal equivalent of %d : %o\n", num, num);
     printf("Hexadecimal equivalent of %d : %X\n", num, num);
