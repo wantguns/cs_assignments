694fb5baeca1dff932b7addf80c100716cf50820
wantguns <therealgunwant@gmail.com>
2019-09-16 12:16:16 +0530

    shamelessly kanged function from a friend

--- assignment_5/6.c
+++ assignment_5/6.c
@@ -1,14 +1,24 @@
 
 /* WAP that accepts a decimal number and find its binary, octal and hexadecimal equivalent and vice
 versa. */
-
+#include <math.h>
 #include <stdio.h>
+
+int binary(int num) {
+    int binary = 0, i = 0;
+    while(num) {
+        binary += (num%2)*pow(10, i);
+        i++;
+        num /= 2;
+    }
+    return binary;
+}
 
 int main() {
     int num;
     printf("Enter a number : ");
     scanf("%d", &num);
-    //printf("Binary equivalent of %d =  ")
+    printf("Binary equivalent of %d : %d\n", num, binary(num));
     printf("Octal equivalent of %d : %o\n", num, num);
     printf("Hexadecimal equivalent of %d : %X\n", num, num);
 }
